66e197be7501d364bccfcd15164a317ee427c1af
Ilya Matveev01~ <aorn666@gmail.com>
2022-10-22 15:58:01 +0300

    как то просто чувствую подвох

---
+++ task41/Program.cs
@@ -0,0 +1,16 @@
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Введите кол-во чисел: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = 0;
+int a = 0;
+for (int i = 0; i < m; i++)
+{
+    Console.Write("Введите число: ");
+    n = Convert.ToInt32(Console.ReadLine());
+     if (n>0)
+     {
+        a++;
+     }
+}
+Console.WriteLine(a);
